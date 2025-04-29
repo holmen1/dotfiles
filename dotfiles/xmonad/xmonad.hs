568dd756ac20db2cc0846ee13f28e5ab61f17fae
holmen1 <holmen1@gmail.com>
2025-04-30 00:53:39 +0200

    machineSpecificKeys no work

--- dotfiles/xmonad/xmonad.hs
+++ dotfiles/xmonad/xmonad.hs
@@ -5,6 +5,8 @@ import XMonad.Hooks.StatusBar.PP
 import XMonad.Hooks.FadeWindows
 import XMonad.Actions.CycleWS
 import XMonad.Hooks.EwmhDesktops (ewmhFullscreen, ewmh)
+import System.IO.Unsafe (unsafePerformIO)
+import System.Environment (lookupEnv)
 
 myModMask       = mod4Mask -- Rebind Mod to the Super key
 myTerminal      = "kitty"
@@ -14,16 +16,19 @@ myMagenta       = "#FF00FF"
 myCyan          = "#00FFFF"
 
 main :: IO ()
-main = xmonad
-     . ewmhFullscreen
-     . ewmh
-     . withEasySB (statusBarProp "xmobar" (pure myXmobarPP)) toggleStrutsKey
-     $ myConfig
+main = do 
+    specificKeys <- machineSpecificKeys
+    print (map fst specificKeys) -- Debug: Print only the keybindings
+    xmonad
+      . ewmhFullscreen
+      . ewmh
+      . withEasySB (statusBarProp "xmobar" (pure myXmobarPP)) toggleStrutsKey
+      $ myConfig specificKeys
   where
     toggleStrutsKey :: XConfig Layout -> (KeyMask, KeySym)
     toggleStrutsKey XConfig{ modMask = m } = (m, xK_b)
 
-myConfig = def
+myConfig additionalKeys' = def
     { modMask    = myModMask,
       terminal   = myTerminal,
       workspaces = myWorkspaces,
@@ -36,12 +41,28 @@ myConfig = def
     , ((myModMask, xK_f                   ), spawn myBrowser)
     , ((myModMask .|. shiftMask, xK_Return), spawn myTerminal)
     , ((myModMask, xK_c                   ), kill)
-    , ((0, xK_F9 ), spawn "brightnessctl set 10%-")  -- Decrease brightness
-    , ((0, xK_F10), spawn "brightnessctl set +10%")  -- Increase brightness
+    -- , ((0, xK_F6 ), spawn "brightnessctl set 10%-")  -- Decrease brightness
+    -- , ((0, xK_F7), spawn "brightnessctl set +10%")  -- Increase brightness
     , ((myModMask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
     , ((myModMask, xK_Tab), nextWS)  -- Cycle to the next workspace
     -- , ((myModMask .|. shiftMask, xK_s), unGrab *> spawn "scrot -s ~/Downloads/screenshot_%Y-%m-%d_%H-%M-%S.png")
-    ]
+    ] ++ additionalKeys'
+
+-- Define machine-specific keybindings based on HOSTNAME
+machineSpecificKeys :: IO [((KeyMask, KeySym), X ())]
+machineSpecificKeys = do
+    hostname <- lookupEnv "HOSTNAME"
+    print hostname -- Debug: Print the hostname
+    appendFile "/tmp/xmonad-debug.log" ("Hostname: " ++ show hostname ++ "\n") -- Log hostname
+    return $ case hostname of
+        Just "xps" -> [ ((0, xK_F6), spawn "brightnessctl set 10%-")
+                      , ((0, xK_F7), spawn "brightnessctl set +10%")
+                      ]
+        Just "hp"  -> [ ((0, xK_F9), spawn "brightnessctl set 10%-")
+                      , ((0, xK_F10), spawn "brightnessctl set +10%")
+                      ]
+        _ -> [] -- Default to no brightness keys if HOSTNAME is not set or unrecognized
+
 myWorkspaces = map show [1..5]
 myLayoutHook = tiled ||| Mirror tiled ||| Full
   where
@@ -54,7 +75,6 @@ myFadeHook = composeAll [ opaque, isUnfocused --> transparency 0.8 ]
 myXmobarPP :: PP
 myXmobarPP = def
     { ppSep   = xmobarColor myMagenta "" $ ppSep def -- Apply magenta to the default separators
-     
      , ppTitleSanitize   = xmobarStrip
      , ppCurrent         = xmobarColor myCyan ""
     , ppOrder = \[ws, l, wins] -> [l, ws, wins]
