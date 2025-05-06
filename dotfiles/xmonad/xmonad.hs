8dd27f3c4f2a3c6f765694bc8a7df3c38b3b86c0
holmen1 <holmen1@gmail.com>
2025-05-06 07:11:08 +0200

    add workspace notification

--- dotfiles/xmonad/xmonad.hs
+++ dotfiles/xmonad/xmonad.hs
@@ -7,6 +7,11 @@ import XMonad.Actions.CycleWS
 import XMonad.Hooks.EwmhDesktops (ewmhFullscreen, ewmh)
 import System.Environment (lookupEnv, getExecutablePath)
 import System.FilePath (takeDirectory, (</>))
+import qualified XMonad.StackSet as W
+import XMonad.Prompt
+import XMonad.Prompt.Workspace
+--import XMonad.Util.NamedWindows (getName)
+--import XMonad.Util.WorkspaceCompare (getSortByIndex)
 
 myModMask       = mod4Mask -- Rebind Mod to the Super key
 myFileManager   = "thunar"
@@ -47,6 +52,7 @@ myConfig additionalKeys' = def
     , ((myModMask, xK_Tab                 ), nextWS)  -- Cycle to the next workspace
     , ((myModMask, xK_s                   ), spawn "scrot ~/Downloads/screenshot_%Y-%m-%d_%H-%M-%S.png")
     -- , ((myModMask .|. shiftMask, xK_s), unGrab *> spawn "scrot -s ~/Downloads/screenshot_%Y-%m-%d_%H-%M-%S.png")
+    , ((myModMask, xK_BackSpace), showWorkspaceStatus)
     ] ++ additionalKeys'
 
 -- Define machine-specific keybindings based on HOSTNAME
@@ -79,6 +85,30 @@ myLayoutHook = tiled ||| Mirror tiled ||| Full
     ratio    = 5/8    -- Default proportion of screen occupied by master pane
     delta    = 3/100  -- Percent of screen to increment by when resizing panes
 myFadeHook = composeAll [ opaque, isUnfocused --> transparency 0.8 ]
+
+showWorkspaceStatus :: X ()
+showWorkspaceStatus = do
+    ws <- gets windowset
+    
+    let current = W.currentTag ws
+        -- Hidden workspaces WITH windows
+        hiddenOccupied = [W.tag w | w <- W.hidden ws, 
+                           maybe False (const True) (W.stack w)]
+        
+        -- Empty workspaces
+        emptyWs = [W.tag w | w <- W.workspaces ws, 
+                   maybe True (const False) (W.stack w),
+                   W.tag w /= current]
+        
+        -- Build status message (only show non-empty sections)
+        status = "Current: " ++ current ++ 
+                 (if not (null hiddenOccupied)
+                     then "\nHidden: " ++ unwords hiddenOccupied
+                     else "")
+                 
+    -- Use timeout (-t) of 1500ms and prevent title truncation with proper flags
+        -- Use timeout (-t) of 1500ms and prevent title truncation with proper flags
+    spawn $ "notify-send -t 800 -u normal -h string:x-canonical-private-synchronous:ws 'WS' '" ++ status ++ "'"
 
 myXmobarPP :: PP
 myXmobarPP = def
