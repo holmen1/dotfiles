68dcec204ea465a9fc307a50fc2b1b8dcb00f762
holmen1 <holmen1@gmail.com>
2025-05-19 21:04:35 +0200

    move and follow

--- dotfiles/xmonad/xmonad.hs
+++ dotfiles/xmonad/xmonad.hs
@@ -29,7 +29,7 @@ myConfig = def
       focusedBorderColor = myMagenta
     }
   `additionalKeys`
-    [ ((myModMask, xK_a                   ), spawn myAppLauncher)
+    ([((myModMask, xK_a                   ), spawn myAppLauncher)
     , ((myModMask, xK_w                   ), spawn myBrowser)
     , ((myModMask, xK_f                   ), spawn "firefox")
     , ((myModMask, xK_e                   ), spawn myFileManager)
@@ -40,6 +40,13 @@ myConfig = def
     , ((myModMask, xK_s                   ), spawn "scrot ~/Downloads/screenshot_%Y-%m-%d_%H-%M-%S.png")
     , ((myModMask .|. shiftMask, xK_s     ), unGrab >> spawn "scrot -s ~/Downloads/screenshot_%Y-%m-%d_%H-%M-%S.png")
     ]
+    ++
+    -- mod-[1..9], Switch to workspace N
+    -- mod-shift-[1..9], Move client to workspace N and follow it
+    [((m .|. myModMask, k), windows $ f i)
+        | (i, k) <- zip myWorkspaces [xK_1 .. xK_9]
+        , (f, m) <- [(W.greedyView, 0), (\i -> \w -> W.greedyView i (W.shift i w), shiftMask)]
+    ])
 
 myWorkspaces = map show [1..6]
 myLayoutHook = tiled ||| Mirror tiled ||| Full
