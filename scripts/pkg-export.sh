d6f67de69483b2e8a6053a5f3a437ab18cfbdb30
holmen1 <holmen1@gmail.com>
2026-01-04 10:25:37 +0100

    refactor bsd package export

--- scripts/pkg-export.sh
+++ scripts/pkg-export.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 # FreeBSD
+
+[ $# -eq 0 ] && { echo "Usage: $0 <output-directory>"; exit 1; }
+
 # Export manually installed packages
+pkg query -e '%a = 0' '%R %o' | awk '$1 == "FreeBSD-ports" {print $2}' > "$1/pkglist.txt"
+echo "Exported list of explicitly installed packages to $1/pkglist.txt"
 
-[ $# -eq 0 ] && { echo "Usage: $0 <output>"; exit 1; }
-
-pkg query -e '%a = 0' '%R %o' | awk '$1 == "FreeBSD-ports" {print $2}' > "$1"
-echo "Exported to $1"
+# Export packages built from ports
+pkg query -e '%a = 0' '%R %o' | awk '$1 != "FreeBSD-ports" && $1 != "FreeBSD-base"  {print $2}' > "$1/foreignpkglist.txt"
+echo "Exported list of ports to $1/foreignpkglist.txt"
--- scripts/ports-export.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# FreeBSD
-# Export packages built from ports
-
-[ $# -eq 0 ] && { echo "Usage: $0 <output>"; exit 1; }
-
-pkg query -e '%a = 0' '%R %o' | awk '$1 != "FreeBSD-ports" && $1 != "FreeBSD-base"  {print $2}' > "$1"
-echo "Exported to $1"
