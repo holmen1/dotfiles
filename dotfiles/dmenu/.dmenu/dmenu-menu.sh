da51add00ab64911428ae1fa090ee1aa5f01fecf
holmen1 <holmen1@gmail.com>
2026-04-09 22:02:37 +0200

    dmenu: bash help

--- dotfiles/dmenu/.dmenu/dmenu-menu.sh
+++ dotfiles/dmenu/.dmenu/dmenu-menu.sh
@@ -5,6 +5,7 @@ DOTFILES=$HOME/repos/dotfiles
 DSCRIPT=$HOME/.dmenu
 XKB_STATE="$HOME/.cache/xkb-layout"
 XKB_README="$DOTFILES/dotfiles/xkb/README.md"
+BASH_README="$DOTFILES/dotfiles/bash/README.md"
 MONITOR_SCRIPTS=$DOTFILES/scripts
 
 
@@ -21,16 +22,22 @@ ssid=$($DSCRIPT/dmenu-wifi.sh --get-ssid)
 vpn=$($DSCRIPT/dmenu-mullvad.sh --get-location)
 
 # Main categories
-category=$(printf "Help\nNetwork\nXKB" | dmenu -i -p "xkb[$current_xkb]:wifi[$ssid]:vpn[$vpn]:bat[$battery_level%]" \
+category=$(printf "Help\nNetwork\nXKB" | dmenu -i -p "x[$current_xkb] w[$ssid] v[$vpn] b[$battery_level%]" \
 -nb "#222222" -nf "#ffffff" -sb "#A300A3" -sf "#ffffff" \
 -fn "$FONT")
 
 case "$category" in
   "Help")
-    app=$(printf "XKB\nlf\nXmonad" | dmenu -i -p "App:" -nb "#222222" -nf "#ffffff" -sb "#A300A3" -sf "#ffffff" -fn "$FONT")
+    app=$(printf "XKB\nlf\nbash\nXmonad" | dmenu -i -p "App:" -nb "#222222" -nf "#ffffff" -sb "#A300A3" -sf "#ffffff" -fn "$FONT")
     case "$app" in
       "XKB")
-        sed -n 9,34p "$XKB_README" | dmenu -l 26 -p "XKB Help" -nb "#222222" -nf "#ffffff" -sb "#222222" -sf "#ffffff" -fn "$FONT" ;;
+        sed -n 9,34p "$XKB_README" | \
+		dmenu -l 26 -p "XKB Help" -nb "#222222" -nf "#ffffff" -sb "#222222" -sf "#ffffff" \
+		-fn "$FONT" ;;
+      "bash")
+        sed -n 16,26p "$BASH_README" | \
+		dmenu -l 11 -p "bash Help" -nb "#222222" -nf "#ffffff" -sb "#222222" -sf "#ffffff" \
+		-fn "$FONT" ;;
       "Xmonad")
         $DSCRIPT/dmenu-help-xmonad.sh ;;
       "lf")
