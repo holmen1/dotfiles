3ae59c0139ef0fa86a1a07541734cfd9de767831
holmen1 <holmen1@gmail.com>
2026-04-11 10:39:44 +0200

    ctd

--- dotfiles/dmenu/.dmenu/dmenu-wifi.sh
+++ dotfiles/dmenu/.dmenu/dmenu-wifi.sh
@@ -7,23 +7,23 @@ fc-list | grep -qi "JetBrainsMono Nerd Font" \
     && FONT="JetBrainsMono Nerd Font Mono-14" \
     || FONT="monospace-14"
 
-menu() { dmenu -p "$1" ${2:+-l "$2"} -nb "#222222" -nf "#ffffff" -sb "#A300A3" -sf "#ffffff" -fn "$FONT"; }
+menu() { dmenu -i -p "$1" ${2:+-l "$2"} -nb "#222222" -nf "#ffffff" -sb "#A300A3" -sf "#ffffff" -fn "$FONT"; }
 
 
 action=$(printf "Scan\nManual\nDisconnect" | menu "WiFi:")
 case "$action" in
-	Disconnect) "$MONITOR_WIFI_SCRIPT" --disconnect ;;
-        Scan)
-          selected=$("$MONITOR_WIFI_SCRIPT" --scan | menu "Networks:")
-          [ -n "$selected" ] && "$MONITOR_WIFI_SCRIPT" --connect "$selected"
-        ;;
-        Manual)
-          ssid=$(echo "" | menu "SSID:")
-          if [ -n "$ssid" ]; then
-	    passwd=$(menu "Password for $ssid:")
-	    "$MONITOR_WIFI_SCRIPT" --connect "$ssid" "$passwd"
-          fi
-        ;;
+  Disconnect) "$MONITOR_WIFI_SCRIPT" --disconnect ;;
+  Scan)
+    selected=$("$MONITOR_WIFI_SCRIPT" --scan | menu "Networks:")
+      [ -n "$selected" ] && "$MONITOR_WIFI_SCRIPT" --connect "$selected"
+    ;;
+    Manual)
+      ssid=$(echo "" | menu "SSID:")
+        if [ -n "$ssid" ]; then
+          passwd=$(menu "Password for $ssid:")
+            "$MONITOR_WIFI_SCRIPT" --connect "$ssid" "$passwd"
+        fi
+    ;;
 esac
 
 
