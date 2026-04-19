5fbcbf921645775bdac43d2163760131e3e5febd
holmen1 <holmen1@gmail.com>
2026-04-19 10:16:43 +0200

    debian install

---
+++ install/debianinstall/configure_build_install_link.sh
@@ -0,0 +1,165 @@
+#!/bin/sh
+
+USER=$(whoami)
+EMAIL=$USER@gmail.com
+
+DOTFILES_DIR=~/repos/dotfiles
+SCRIPTS_DIR=$DOTFILES_DIR/scripts
+INSTALL_SCRIPT=$SCRIPTS_DIR/install-pacman.sh
+LINK_SCRIPT=$SCRIPTS_DIR/link_config.sh
+
+XMONAD_DIR=$DOTFILES_DIR/install/build/xmonad
+ST_DIR=$DOTFILES_DIR/install/build/st
+XKB_DIR=$DOTFILES_DIR/install/build/xkb
+
+COMPUTERNAME=$(hostnamectl --static 2>/dev/null || hostname -s)
+PKGPROFILE=${COMPUTERNAME}
+PKGLIST=$DOTFILES_DIR/install/archinstall/packages/$PKGPROFILE/pkglist.txt
+FPKGLIST=$DOTFILES_DIR/install/archinstall/packages/$PKGPROFILE/foreignpkglist.txt
+LINKS=$DOTFILES_DIR/install/archinstall/links/$PKGPROFILE/links.config
+
+TEST=$DOTFILES_DIR/install/archinstall/tests/$PKGPROFILE/sanity_check.sh
+
+sudo pacman -S --needed openssh
+sudo -k
+
+read -p "Configure git? [y/N] " ans
+case "$ans" in
+    [Yy]*)
+    # Configure git
+    git config --global user.name "$USER"
+    git config --global user.email "$EMAIL"
+    ;;
+esac
+
+read -p "Generate SSH key? [y/N] " ans
+case "$ans" in
+    [Yy]*)
+# Generate ssh key
+    cd ~
+    mkdir -p .ssh
+    chmod 700 .ssh
+    ssh-keygen -t ed25519 -C "$EMAIL" -f ~/.ssh/id_ed25519
+    eval "$(ssh-agent -s)"
+    ssh-add ~/.ssh/id_ed25519
+    ;;
+esac
+
+read -p "Install yay? [y/N] " ans
+case "$ans" in
+    [Yy]*)
+    # Install yay
+    mkdir -p tmp
+    cd tmp
+    sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+    sudo -k
+    cd ..
+    ;;
+esac
+
+# Install packages
+echo "$PKGLIST"
+read -p "Install pkglist? [y/N] " ans
+case "$ans" in
+    [Yy]*)
+    $INSTALL_SCRIPT $PKGLIST
+    ;;
+esac
+
+echo "$FPKGLIST"
+read -p "Install foreignpkglist? [y/N] " ans
+case "$ans" in
+    [Yy]*)
+    $INSTALL_SCRIPT $FPKGLIST
+    ;;
+esac
+
+read -p "Build xmonad? [y/N] " ans
+case "$ans" in
+    [Yy]*)
+    # Build Xmonad
+    $XMONAD_DIR/build-xmonad.sh
+    ;;
+esac
+
+read -p "Install xmonad? [y/N] " ans
+case "$ans" in
+    [Yy]*)
+    sudo mkdir -p /opt/xmonad
+    # Backup existing files instead of removing
+    for file in /opt/xmonad/*; do
+        if [ -e "$file" ] && [[ ! "$file" == *.bak ]]; then
+            sudo mv "$file" "${file}.bak"
+        fi
+    done
+
+    sudo cp -f $XMONAD_DIR/bin/xmonad-v0.18.[0-9] /opt/xmonad/
+    echo "Installed xmonad to /opt/xmonad/"
+    # Link to the latest installed version, ignoring .bak files
+    LATEST_XMONAD=$(ls -v /opt/xmonad/xmonad-v0.18.[0-9] | grep -v "\.bak$" | tail -n 1)
+    if [ -n "$LATEST_XMONAD" ]; then
+        sudo ln -sf "$LATEST_XMONAD" /usr/local/bin/xmonad
+        echo "Created symlink for xmonad pointing to $LATEST_XMONAD"
+    fi
+    sudo -k
+    ;;
+esac
+
+read -p "Build st? [y/N] " ans
+case "$ans" in
+    [Yy]*)
+    # Build st
+    $ST_DIR/build-st.sh
+    ;;
+esac
+
+read -p "Install st? [y/N] " ans
+case "$ans" in
+    [Yy]*)
+    sudo mkdir -p /opt/st
+    sudo rm -f /opt/st/*
+
+    sudo cp -f $ST_DIR/bin/st-0.9.[0-9] /opt/st/
+    echo "Installed st to /opt/st/"
+    sudo ln -sf /opt/st/st-0.9.* /usr/local/bin/st
+    sudo -k
+    echo "Created symlink for st"
+    ;;
+esac
+
+read -p "Build xkb keymap? [y/N] " ans
+case "$ans" in
+    [Yy]*)
+    $XKB_DIR/build-xkb.sh
+    echo "Built xkb keymap"
+    ;;
+esac
+
+read -p "Link dotfiles? [y/N] " ans
+case "$ans" in
+    [Yy]*)
+    # Link dotfiles
+    $LINK_SCRIPT $LINKS
+    ;;
+esac
+
+read -p "Enable services? [y/N] " ans
+case "$ans" in
+    [Yy]*)
+    # Enable services
+    systemctl --user daemon-reload
+    systemctl --user enable --now system-monitor.timer
+    ;;
+esac
+
+read -p "Run tests? [Y/n] " ans
+case "$ans" in
+    [Nn])
+    # Skip tests
+    ;;
+    *)
+    # Test (default)
+    $TEST
+    ;;
+esac
+
