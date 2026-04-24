d983f20ed2cd03855c186d5007a752f6e29a9dbc
holmen1 <holmen1@gmail.com>
2026-04-24 19:50:21 +0200

    debian: neovim build 0.12

--- install/build/neovim/build-neovim.sh
+++ install/build/neovim/build-neovim.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 set -e
 
-NVIM_TAG="nightly"
+NVIM_TAG="stable"
 NV_DIR="${HOME}/repos/dotfiles/install/build/neovim/neovim"
 
 git clone https://github.com/neovim/neovim $NV_DIR
 cd $NV_DIR
 git checkout $NVIM_TAG
 # gmake required on FreeBSD (BSD make is incompatible); on Linux/macOS use make
-gmake CMAKE_BUILD_TYPE=Release
-sudo gmake install
+#gmake CMAKE_BUILD_TYPE=Release
+#sudo gmake install
+make CMAKE_BUILD_TYPE=Release
+sudo make install
 
 echo "Done: $(nvim --version | head -1)"
