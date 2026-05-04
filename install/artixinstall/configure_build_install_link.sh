33f33835cff57ede9df7ebf2aafc5434fcf2ef0d
holmen1 <holmen1@gmail.com>
2026-05-04 23:51:04 +0200

    openrc adapt

--- install/artixinstall/configure_build_install_link.sh
+++ install/artixinstall/configure_build_install_link.sh
@@ -7,6 +7,10 @@ DOTFILES_DIR=~/repos/dotfiles
 INSTALL_SCRIPT=$DOTFILES_DIR/install/artixinstall/scripts/install-pacman.sh
 LINK_SCRIPT=$DOTFILES_DIR/scripts/link_config.sh
 
+XMONAD_DIR=$DOTFILES_DIR/install/build/xmonad
+ST_DIR=$DOTFILES_DIR/install/build/st
+XKB_DIR=$DOTFILES_DIR/install/build/xkb
+
 COMPUTERNAME=$(hostname -s)
 PKGPROFILE=${COMPUTERNAME}
 PKGLIST=$DOTFILES_DIR/install/artixinstall/packages/$PKGPROFILE/pkglist.txt
@@ -65,6 +69,60 @@ case "$ans" in
     ;;
 esac
 
+read -p "Build xmonad? [y/N] " ans
+case "$ans" in
+    [Yy]*)
+    $XMONAD_DIR/build-xmonad.sh
+    ;;
+esac
+
+read -p "Install xmonad? [y/N] " ans
+case "$ans" in
+    [Yy]*)
+    sudo mkdir -p /opt/xmonad
+    for file in /opt/xmonad/*; do
+        case "$file" in
+            *.bak) ;;
+            *) [ -e "$file" ] && sudo mv "$file" "${file}.bak" ;;
+        esac
+    done
+    sudo cp -f $XMONAD_DIR/bin/xmonad-v0.18.[0-9] /opt/xmonad/
+    LATEST_XMONAD=$(ls -v /opt/xmonad/xmonad-v0.18.[0-9] | grep -v '\.bak$' | tail -n 1)
+    if [ -n "$LATEST_XMONAD" ]; then
+        sudo ln -sf "$LATEST_XMONAD" /usr/local/bin/xmonad
+        echo "Created symlink for xmonad -> $LATEST_XMONAD"
+    fi
+    sudo -k
+    ;;
+esac
+
+read -p "Build st? [y/N] " ans
+case "$ans" in
+    [Yy]*)
+    $ST_DIR/build-st.sh
+    ;;
+esac
+
+read -p "Install st? [y/N] " ans
+case "$ans" in
+    [Yy]*)
+    sudo mkdir -p /opt/st
+    sudo rm -f /opt/st/*
+    sudo cp -f $ST_DIR/bin/st-0.9.[0-9] /opt/st/
+    sudo ln -sf /opt/st/st-0.9.* /usr/local/bin/st
+    sudo -k
+    echo "Created symlink for st"
+    ;;
+esac
+
+read -p "Build xkb keymap? [y/N] " ans
+case "$ans" in
+    [Yy]*)
+    $XKB_DIR/build-xkb.sh
+    echo "Built xkb keymap"
+    ;;
+esac
+
 read -p "Link dotfiles? [y/N] " ans
 case "$ans" in
     [Yy]*)
@@ -78,6 +136,10 @@ case "$ans" in
     # OpenRC — add services at default runlevel
     sudo rc-update add iwd default
     sudo rc-update add dbus default
+    sudo rc-update add elogind default
+    sudo rc-update add cronie default
+    sudo rc-service cronie start
+    crontab ~/.config/cron/user.crontab
     echo "Services added to default runlevel"
     ;;
 esac
