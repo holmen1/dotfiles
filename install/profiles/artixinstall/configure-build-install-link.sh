a06de0fe2992862dd8ce5cad6c42c9156453dfa2
holmen1 <holmen1@gmail.com>
2026-08-13 12:10:23 +0200

    xmonad: install script in build dir

--- install/profiles/artixinstall/configure-build-install-link.sh
+++ install/profiles/artixinstall/configure-build-install-link.sh
@@ -55,20 +55,7 @@ esac
 read -p "Install xmonad? [y/N] " ans
 case "$ans" in
     [Yy]*)
-    sudo mkdir -p /opt/xmonad
-    for file in /opt/xmonad/*; do
-        case "$file" in
-            *.bak) ;;
-            *) [ -e "$file" ] && sudo mv "$file" "${file}.bak" ;;
-        esac
-    done
-    sudo cp -f $XMONAD_DIR/bin/xmonad-0.18.[0-9] /opt/xmonad/
-    LATEST_XMONAD=$(ls -v /opt/xmonad/xmonad-0.18.[0-9] | grep -v '\.bak$' | tail -n 1)
-    if [ -n "$LATEST_XMONAD" ]; then
-        sudo ln -sf "$LATEST_XMONAD" /usr/local/bin/xmonad
-        echo "Created symlink for xmonad -> $LATEST_XMONAD"
-    fi
-    sudo -k
+    $XMONAD_DIR/install-xmonad.sh
     ;;
 esac
 
